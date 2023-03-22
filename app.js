7ac5b75bf6c4880691557774accff367ce8ed5a3
softenglj <124595944+softenglj@users.noreply.github.com>
2023-03-23 07:47:34 +0800

    Add files via upload
    Signed-off-by: softenglj <124595944+softenglj@users.noreply.github.com>

---
+++ app.js
@@ -0,0 +1,20 @@
+function updateClock() {
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    document.getElementById("clock").innerHTML = hours + ":" + minutes + ":" + seconds;
+}
+
+setInterval(updateClock, 1000);
